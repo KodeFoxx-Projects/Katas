9458059a23d3f905b171c7716f7f3509553c7fec
Yves Schelpe <yves.schelpe@gmail.com>
2018-04-14 12:16:26 +0200

    For readability, converted all 2d arrays to ConvertIntegerArrayToBoard

--- Source/Katas/FourInARow/Kodefoxx.Katas.FourInARow.Tests/Board/BoardGridHelper.cs
+++ Source/Katas/FourInARow/Kodefoxx.Katas.FourInARow.Tests/Board/BoardGridHelper.cs
@@ -8,49 +8,22 @@ namespace Kodefoxx.Katas.FourInARow.Tests.Board
     public static class BoardGridHelper
     {
         public static IBoardGrid CreateFourByFourBoard()
-            => new BoardGrid(new[,]
+            => ConvertIntegerArrayToBoard(new[,]
             {
-                {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.Empty},
-                {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.Empty},
-                {BoardSlotValue.Empty, BoardSlotValue.P2, BoardSlotValue.P2, BoardSlotValue.Empty},
-                {BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.P2, BoardSlotValue.P2}
+                {0, 0, 1, 0},
+                {0, 0, 1, 0},
+                {0, 2, 2, 0},
+                {0, 1, 2, 2}
             });
 
         public static IBoardGrid CreateFullBoard()
-            => new BoardGrid(new[,]
+            => ConvertIntegerArrayToBoard(new[,]
             {
-                {BoardSlotValue.P1, BoardSlotValue.P2, BoardSlotValue.P1, BoardSlotValue.P2},
-                {BoardSlotValue.P2, BoardSlotValue.P2, BoardSlotValue.P1, BoardSlotValue.P2},
-                {BoardSlotValue.P1, BoardSlotValue.P2, BoardSlotValue.P2, BoardSlotValue.P1},
-                {BoardSlotValue.P1, BoardSlotValue.P1, BoardSlotValue.P2, BoardSlotValue.P2}
-            });
-
-        public static IBoardGrid ConvertIntegerArrayToBoard(int[,] integerArray)
-        {
-            var boardGridSlotValues = new BoardSlotValue[integerArray.GetLength(0), integerArray.GetLength(1)];
-
-            foreach (var i in Enumerable.Range(0, integerArray.GetLength(0)))
-            {
-                foreach (var j in Enumerable.Range(0, integerArray.GetLength(1)))
-                {
-                    switch (integerArray[i, j])
-                    {
-                        case 1:
-                            boardGridSlotValues[i, j] = BoardSlotValue.P1;
-                            break;
-                        case 2:
-                            boardGridSlotValues[i, j] = BoardSlotValue.P2;
-                            break;
-                        default:
-                            boardGridSlotValues[i, j] = BoardSlotValue.Empty;
-                            break;
-                    }
-                }
-            }
-
-            var boardGrid = new BoardGrid(boardGridSlotValues);
-            return boardGrid;
-        }
+                {1, 2, 1, 2},
+                {2, 2, 1, 2},
+                {1, 2, 2, 1},
+                {1, 1, 2, 2}
+            });        
 
         public static (BoardSlotValue Winner, IEnumerable<(WinMethod WinMethod, IBoardGrid Board)> Boards)
             CreateWinningBoardGridForPlayerOne() => (
@@ -58,72 +31,72 @@ namespace Kodefoxx.Katas.FourInARow.Tests.Board
             Boards: new List<(WinMethod WinMethod, IBoardGrid Board)> {
                 (
                     WinMethod: WinMethod.None,
-                    Board: new BoardGrid(new[,]
+                    Board: ConvertIntegerArrayToBoard(new[,]
                     {
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty}
+                        {0, 0, 0, 0},
+                        {0, 0, 0, 0},
+                        {0, 0, 0, 0},
+                        {0, 0, 0, 0}
                     })
                 ),
                 (
                     WinMethod: WinMethod.None,
-                    Board: new BoardGrid(new[,]
+                    Board: ConvertIntegerArrayToBoard(new[,]
                     {
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.P2, BoardSlotValue.P2, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.P2, BoardSlotValue.P2}
+                        {0, 0, 1, 0},
+                        {0, 0, 1, 0},
+                        {0, 2, 2, 0},
+                        {0, 1, 2, 2}
                     })
                 ),
                 (
                     WinMethod: WinMethod.Row,
-                    Board: new BoardGrid(new[,]
+                    Board: ConvertIntegerArrayToBoard(new[,]
                     {
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty},
-                        {BoardSlotValue.P1, BoardSlotValue.P1, BoardSlotValue.P1, BoardSlotValue.P1}
+                        {0, 0, 0, 0},
+                        {0, 0, 0, 0},
+                        {0, 0, 0, 0},
+                        {1, 1, 1, 1}
                     })
                 ),
                 (
                     WinMethod: WinMethod.Column,
-                    Board: new BoardGrid(new[,]
+                    Board: ConvertIntegerArrayToBoard(new[,]
                     {
-                        {BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.Empty, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.Empty, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.Empty, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.Empty, BoardSlotValue.Empty}
+                        {0, 1, 0, 0},
+                        {0, 1, 0, 0},
+                        {0, 1, 0, 0},
+                        {0, 1, 0, 0}
                     })
                 ),
                 (
                     WinMethod: WinMethod.DiagonalTopToBottom,
-                    Board: new BoardGrid(new[,]
+                    Board: ConvertIntegerArrayToBoard(new[,]
                     {
-                        {BoardSlotValue.P1, BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.Empty, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.P1}
+                        {1, 0, 0, 0},
+                        {0, 1, 0, 0},
+                        {0, 0, 1, 0},
+                        {0, 0, 0, 1}
                     })
                 ),
                 (
                     WinMethod: WinMethod.DiagonalBottomToTop,
-                    Board: new BoardGrid(new[,]
+                    Board: ConvertIntegerArrayToBoard(new[,]
                     {
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.P1},
-                        {BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.Empty},
-                        {BoardSlotValue.Empty, BoardSlotValue.P1, BoardSlotValue.Empty, BoardSlotValue.Empty},
-                        {BoardSlotValue.P1, BoardSlotValue.Empty, BoardSlotValue.Empty, BoardSlotValue.Empty}
+                        {0, 0, 0, 1},
+                        {0, 0, 1, 0},
+                        {0, 1, 0, 0},
+                        {1, 0, 0, 0}
                     })
                 ),
                 (
                     WinMethod: WinMethod.Draw,
-                    Board: new BoardGrid(new[,]
+                    Board: ConvertIntegerArrayToBoard(new[,]
                     {
-                        {BoardSlotValue.P1, BoardSlotValue.P2, BoardSlotValue.P2, BoardSlotValue.P2},
-                        {BoardSlotValue.P1, BoardSlotValue.P2, BoardSlotValue.P1, BoardSlotValue.P1},
-                        {BoardSlotValue.P2, BoardSlotValue.P1, BoardSlotValue.P1, BoardSlotValue.P1},
-                        {BoardSlotValue.P1, BoardSlotValue.P2, BoardSlotValue.P1, BoardSlotValue.P1}
+                        {1, 2, 2, 2},
+                        {1, 2, 1, 1},
+                        {2, 1, 1, 1},
+                        {1, 2, 1, 1}
                     })
                 ),
                 (
@@ -152,5 +125,36 @@ namespace Kodefoxx.Katas.FourInARow.Tests.Board
                 ),
             }
         );
+
+        /// <summary>
+        /// Converts a 2d <see cref="int"/> <paramref name="integerArray"/> to a 2d <see cref="BoardSlotValue"/> array and makes a <see cref="IBoardGrid"/> out of it.
+        /// </summary>
+        /// <param name="integerArray">The 2d array of <see cref="int"/>s</param>        
+        public static IBoardGrid ConvertIntegerArrayToBoard(int[,] integerArray)
+        {
+            var boardGridSlotValues = new BoardSlotValue[integerArray.GetLength(0), integerArray.GetLength(1)];
+
+            foreach (var i in Enumerable.Range(0, integerArray.GetLength(0)))
+            {
+                foreach (var j in Enumerable.Range(0, integerArray.GetLength(1)))
+                {
+                    switch (integerArray[i, j])
+                    {
+                        case 1:
+                            boardGridSlotValues[i, j] = BoardSlotValue.P1;
+                            break;
+                        case 2:
+                            boardGridSlotValues[i, j] = BoardSlotValue.P2;
+                            break;
+                        default:
+                            boardGridSlotValues[i, j] = BoardSlotValue.Empty;
+                            break;
+                    }
+                }
+            }
+
+            var boardGrid = new BoardGrid(boardGridSlotValues);
+            return boardGrid;
+        }
     }
 }
