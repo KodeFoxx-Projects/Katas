2af4b53386b5ed0833230b57f648525e4cc0bc94
Yves Schelpe <yves.schelpe@gmail.com>
2018-05-02 01:40:38 +0200

    Added basic implementation with timer and a null implementation, mostly for testing purposes.

---
+++ Source/Katas/WordChains/Kodefoxx.Katas.WordChains/Shared/WordChainSolver.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Kodefoxx.Katas.WordChains.Shared.WordLists;
+
+namespace Kodefoxx.Katas.WordChains.Shared
+{    
+    /// <inheritdoc />
+    public abstract class WordChainSolver : IWordChainSolver
+    {
+        /// <summary>
+        /// Gets the <see cref="IWordChain"/>s already found.
+        /// </summary>
+        private readonly ICollection<IWordChain> _wordChains;
+
+        /// <summary>
+        /// Creates a new <see cref="WordChainSolver"/>.
+        /// </summary>
+        protected WordChainSolver()
+            => _wordChains = new List<IWordChain>();
+
+        /// <inheritdoc />
+        public IWordChainSolverResult GetWordChains(IWordList wordList, string startingWord, string endingWord)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var result = TryGetWordChains(wordList, startingWord, endingWord, _wordChains.ToList().AsReadOnly());
+
+            while (result.CanContinue)
+            {
+                if (result.NewlyFoundWordChain != null)
+                {
+                    stopwatch.Stop();
+
+                    _wordChains.Add(WordChain.New(result.NewlyFoundWordChain, stopwatch.Elapsed));
+
+                    stopwatch.Start();
+                    result = TryGetWordChains(wordList, startingWord, endingWord, _wordChains.ToList().AsReadOnly());
+                }
+                else                
+                    stopwatch.Stop();                
+            }
+
+            return WordChainSolverResult.New(_wordChains);
+        }
+
+        /// <summary>
+        /// Tries to get a word chain from the given wordlist.
+        /// </summary>
+        /// <param name="wordList">The list of words (or dictionary) to be used.</param>
+        /// <param name="startingWord">The word that marks the start of the <see cref="IWordChain"/>.</param>
+        /// <param name="endingWord">The word that marks the end of the <see cref="IWordChain"/>.</param>
+        /// <param name="foundWordChains">The <see cref="IWordChain"/>s that were already found.</param>        
+        protected abstract (bool CanContinue, IEnumerable<string> NewlyFoundWordChain) TryGetWordChains(IWordList wordList, string startingWord, string endingWord, 
+            IReadOnlyCollection<IWordChain> foundWordChains);
+    }
+}
---
+++ Source/Katas/WordChains/Kodefoxx.Katas.WordChains/Strategies/NullWordChainSolver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Kodefoxx.Katas.WordChains.Shared;
+using Kodefoxx.Katas.WordChains.Shared.WordLists;
+
+namespace Kodefoxx.Katas.WordChains.Strategies
+{    
+    /// <inheritdoc />
+    public sealed class NullWordChainSolver : WordChainSolver
+    {
+        /// <inheritdoc />
+        protected override (bool CanContinue, IEnumerable<string> NewlyFoundWordChain) TryGetWordChains(
+            IWordList wordList, string startingWord, string endingWord,
+            IReadOnlyCollection<IWordChain> foundWordChains
+        ) => (CanContinue: false, NewlyFoundWordChain: null);
+    }
+}
